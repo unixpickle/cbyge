91444b3a5b8e1908da04ca01fad5d2f54fe16ebf
Alex Nichol <unixpickle@gmail.com>
2021-01-29 17:20:22 -0500

    start working on color picker

--- server/assets/controls.js
+++ server/assets/controls.js
@@ -56,7 +56,6 @@
                 max: 100,
                 value: brightness,
             });
-            this.slider.value = brightness;
             this.content.appendChild(this.slider);
 
             this.label = makeElem('label', 'popup-slider-label', {
@@ -75,8 +74,83 @@
         }
     }
 
+    class ColorPopup {
+        constructor(status) {
+            super();
+
+            this.onRGB = (_value) => null;
+            this.onTone = (_value) => null;
+
+            this.tabs = makeElem('div', 'popup-tabs');
+            this.toneTab = makeElem('button', 'popup-tabs-tab', { textContent: 'Tone' });
+            this.rgbTab = makeElem('button', 'popup-tabs-tab', { textContent: 'RGB' });
+            this.tabs.appendChild(this.toneTab);
+            this.tabs.appendChild(this.rgbTab);
+            this.content.appendChild(this.tabs);
+            this.toneTab.addEventListener('click', () => this.showTab(false));
+            this.rgbTab.addEventListener('click', () => this.showTab(true));
+
+            this.tonePane = makeElem('div', 'popup-tab-pane');
+            this.toneSlider = makeElem('input', 'popup-slider', {
+                type: 'range',
+                min: 0,
+                max: 100,
+                value: 50,
+            });
+            this.toneLabel = makeElem('input', 'popup-slider-label', { textContent: '50%' });
+            this.toneSlider.addEventListener('input', () => {
+                this.toneLabel.textContent = this.toneSlider.value + '%';
+            });
+            this.tonePane.appendChild(this.toneSlider);
+            this.tonePane.appendChild(this.toneLabel);
+            this.content.appendChild(this.tonePane);
+
+            this.rgbPane = makeElem('div', 'popup-tab-pane');
+            this.rgbInput = makeElem('input', 'popup-color-picker', {
+                type: 'color',
+                value: '#00ff00',
+            });
+            this.rgbPane.appendChild(this.rgbInput);
+            this.content.appendChild(this.rgbPane);
+
+            this.useRGB = status['use_rgb'];
+            if (this.useRGB) {
+                this.rgbInput.value = rgbToHex(status['rgb']);
+            } else {
+                this.toneSlider.value = status['color_tone'];
+                this.toneLabel.value = status['color_tone'] + '%';
+            }
+            this.showTab(status['use_rgb']);
+        }
+
+        showTab(useRGB) {
+            this.useRGB = useRGB;
+            this.toneTab.classList.remove('popup-tabs-selected');
+            this.tonePane.classList.remove('popup-tab-pane-current');
+            this.rgbTab.classList.remove('popup-tabs-selected');
+            this.rgbPane.classList.remove('popup-tab-pane-current');
+            if (this.useRGB) {
+                this.rgbTab.classList.add('popup-tabs-selected');
+                this.rgbPane.classList.add('popup-tab-pane-current');
+            } else {
+                this.toneTab.classList.add('popup-tabs-selected');
+                this.tonePane.classList.add('popup-tab-pane-current');
+            }
+        }
+
+        confirm() {
+            super.confirm();
+            if (this.useRGB) {
+                this.onRGB(hexToRGB(this.rgbInput.value));
+            } else {
+                this.onTone(parseInt(this.toneSlider.value));
+            }
+        }
+    }
+
     window.controlPopups = {
         BrightnessPopup: BrightnessPopup,
+        ColorPopup: ColorPopup,
     }
 
 })();
--- server/assets/util.js
+++ server/assets/util.js
@@ -8,3 +8,22 @@ function makeElem(elem, className, attrs) {
     }
     return res;
 }
+
+function rgbToHex(rgb) {
+    let res = '#';
+    rgb.forEach((x) => {
+        if (x < 0x10) {
+            res += '0';
+        }
+        res += x.toString(16);
+    });
+    return res;
+}
+
+function hexToRGB(hex) {
+    const res = [0, 0, 0];
+    for (let i = 0; i < 3; i++) {
+        res[i] = parseInt(hex.substr(i * 2 + 1, 2), 16);
+    }
+    return res
+}
