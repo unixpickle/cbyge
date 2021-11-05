70752055d61bbd4c0640ff88f4e42c6a61281d67
Alex Nichol <unixpickle@gmail.com>
2021-11-05 10:21:32 -0400

    better 2FA user interface

--- server/assets/js/2fa.js
+++ server/assets/js/2fa.js
@@ -1,11 +1,26 @@
-const btn = document.getElementById('get-code');
-btn.addEventListener('click', () => {
-    btn.style.pointerEvents = 'none';
-    btn.style.opacity = '0.5';
-    btn.textContent = 'Sending...';
-    fetch('/2fa/stage1').then(() => {
-        btn.textContent = 'Sent.';
-    }).catch((err) => {
-        btn.textContent = err;
+(function () {
+
+    const btn = document.getElementById('get-code');
+    const statusElement = document.getElementById('code-status');
+
+    btn.addEventListener('click', () => {
+        btn.classList.add('disabled');
+        statusElement.textContent = 'Sending...'
+        fetch('/2fa/stage1').then(() => {
+            btn.classList.remove('disabled');
+            statusElement.classList.add('success');
+            statusElement.textContent = 'Code sent. Check your email.';
+        }).catch((err) => {
+            btn.classList.remove('disabled');
+            statusElement.classList.add('error');
+            statusElement.textContent = '' + err;
+        });
     });
-});
+
+    const errorMsg = new URL(window.location).searchParams.get('error');
+    if (errorMsg) {
+        statusElement.textContent = errorMsg;
+        statusElement.classList.add('error');
+    }
+
+})();
